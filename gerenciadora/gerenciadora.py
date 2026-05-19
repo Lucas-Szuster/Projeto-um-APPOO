8d70a278d7e16dc707c108293f9576d3e7ab3a63
Lucas Szuster <lucasacszuster@gmail.com>
2026-05-19 20:44:33 -0300

    Atualizei a organização de gerenciadora e adicionei o início de lista de usuários

--- gerenciadora/gerenciadora.py
+++ gerenciadora/gerenciadora.py
@@ -6,17 +6,34 @@ from copy import deepcopy
 
 class gerenciadora:
     def __init__(self):
-        self.lista_areas: list[Area] = []
+        self.__lista_areas: list[Area] = []
+        self.__lista_usuarios: list[Usuario] = []
+
+    # ============
+    # PROPRIEDADES
+    # ============
+
+    @property
+    def lista_areas(self):
+        return deepcopy(self.__lista_areas)
+    
+    @property
+    def lista_usuarios(self):
+        return deepcopy(self.__lista_usuarios)
+
+    # =======
+    # MÉTODOS
+    # =======
 
     def adicionar_area(self, nova_area: Area):
-        self.lista_areas.append(nova_area)
+        self.__lista_areas.append(nova_area)
 
     def buscar_area_por_nome(self, nome_area):
-        lista_de_nomes_de_areas: list[str] = [area.nome for area in self.lista_areas]
+        lista_de_nomes_de_areas: list[str] = [area.nome for area in self.__lista_areas]
         if nome_area not in lista_de_nomes_de_areas:
             raise ValueError("Esta área não existe")
         
-        return self.lista_areas[lista_de_nomes_de_areas.index(nome_area)]
+        return self.__lista_areas[lista_de_nomes_de_areas.index(nome_area)]
 
     def adicionar_evento_em_area(self, nome_area: str, novo_evento: Evento):
         self.buscar_area_por_nome(nome_area).adicionar_evento(novo_evento)
