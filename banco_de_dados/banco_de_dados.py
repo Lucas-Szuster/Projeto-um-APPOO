ffba4dee1ccc74c72047063d838f7a9916b52ed0
Lucas Szuster <lucasacszuster@gmail.com>
2026-05-25 14:45:15 -0300

    criei o básico da função que salva os dados de usuários

--- banco_de_dados/banco_de_dados.py
+++ banco_de_dados/banco_de_dados.py
@@ -4,19 +4,27 @@ from gerenciadora.gerenciadora import Gerenciadora
 
 class BancoDeDados:
     def __init__(self):
-        self.diretorio = 'banco_de_dados'
-        self.nome_do_arquivo = 'banco_de_dados.json'
+        self.enconding = "utf-8"
+        self.diretorio = "banco_de_dados"
+        self.nome_do_arquivo = "banco_de_dados.json"
 
     def ler_dados(self):
-        with open(f'{self.diretorio}/{self.nome_do_arquivo}', 'r', encoding='utf-8') as arq:
+        with open(f'{self.diretorio}/{self.nome_do_arquivo}', 'r', encoding=self.enconding) as arq:
             try:
                 json_puro = json.load(arq)
             except json.JSONDecodeError as e:
                 json_puro = []
 
+    def salvar_dados_usuarios(self, gerenciadora: Gerenciadora):
+        lista_de_dicts_usuarios: list[dict] = [usuario.to_dict() for usuario in gerenciadora.lista_usuarios]
+
+        print(lista_de_dicts_usuarios)
+
+        with open(f'{self.diretorio}/{self.nome_do_arquivo}', 'w', encoding=self.enconding) as arq:
+            json.dump(lista_de_dicts_usuarios, arq, indent=4)
+
     def salvar_dados(self, gerenciadora: Gerenciadora):
-        for usuario in gerenciadora.lista_usuarios:
-            pass
+        self.salvar_dados_usuarios(gerenciadora)
 
 a = BancoDeDados()
 a.ler_dados()
--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from usuario.usuario import Usuario
 from banco_de_dados.banco_de_dados import BancoDeDados
 
 
-usu1 = Usuario('leila', 12, 1, 'hei')
+usu1 = Usuario('leila', 12, 1, 'hei', True)
 
 gen = Gerenciadora()
 gen.adicionar_usuario(usu1)
