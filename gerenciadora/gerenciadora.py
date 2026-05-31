9acaf2518f7ac00b0a5e6e1e57cdd9a275098774
Lucas Szuster <lucasacszuster@gmail.com>
2026-05-31 18:38:55 -0300

    Atualizei o método de remoção de itens em área

--- gerenciadora/gerenciadora.py
+++ gerenciadora/gerenciadora.py
@@ -99,8 +99,10 @@ class Gerenciadora:
     def adicionar_item_em_area(self, nome_area: str, item: str):
         self.buscar_area_por_nome(nome_area).adicionar_item_na_lista(item)
 
+    ##Verificação de usuário já é feita em interface gráfica
+    ##- Só apagar as dependenciias de id_usuario quando for fazer, igual ta em cima
     def remover_item_em_area(self, id_usuario: int, nome_area: str, item: str):
-        if self.checar_tipo_usuario(id_usuario) == False:
+        if self.checar_adm(id_usuario) == False:
             raise TypeError("O usuário não é do tipo ADM! Logo, não pode remover item!")
         
         self.buscar_area_por_nome(nome_area).remover_item_da_lista(item)
